9be74a5e34af6632f77298ca70880763e00bf2e1
Osakpemwokan Alonge <janestra@OsakpemokansMBP.lan>
2022-05-24 19:08:47 -0500

    Made code look nicer

--- game.lua
+++ game.lua
@@ -1,3 +1,4 @@
+--sample boards
 board = {{1,0,0},
 {0,1,0},
 {0,0,1}}
@@ -22,80 +23,83 @@ column = {{1,0,0},
 {1,1,1},
 {1,1,1}}
 
+empty_board = {{nil,nil,nil},
+{nil,nil,nil},
+{nil,nil,nil}}
 
 function checkDiagonal(term, table)
-for k,v in ipairs(table) do
-if (table[k][k]~=term) then
-    return false
+    for k,v in ipairs(table) do
+        if (table[k][k]~=term) then
+            return false
+        end
     end
-end
-return true
+    return true
 end
 
 
 function checkRDiagonal(term, table)
-for k,v in ipairs(table) do
-if (table[(#table+1)-k][k]~=term) then
-    return false
+    for k,v in ipairs(table) do
+        if (table[(#table+1)-k][k]~=term) then
+            return false
+        end
     end
-end
-return true
+    return true
 end
 
 
 function checkAll(term, table)
-for k,v in ipairs(table) do
-for i,v in ipairs(table) do
-if (table[k][i]~=term) then
-    return false
-end
-end
-end
-return true
+    for k,v in ipairs(table) do
+        for i,v in ipairs(table) do
+            if (table[k][i]~=term) then
+                return false
+            end
+        end
+    end
+    return true
 end
 
 
 function checkRow(term, table)
-t = 0
-for k,v in ipairs(table) do
-for i,v in ipairs(table) do
-    if (table[k][i]==term) then
-        t = t+1
+    t = 0
+        for k,v in ipairs(table) do
+            for i,v in ipairs(table) do
+                if (table[k][i]==term) then
+                    t = t+1
+                end
+            end
+        if (t == #table) then
+            return true
+        else
+            t=0
+        end
     end
-end
-if (t == #table) then
-    return true
-else
-    t=0
-end
-end
-return false
+    return false
 end
 
 
 function checkColumn(term, table)
-t = 0
-for k,v in ipairs(table) do
-for i,v in ipairs(table) do
-    if (table[i][k]==term) then
-        t = t+1
+    t = 0
+        for k,v in ipairs(table) do
+            for i,v in ipairs(table) do
+                if (table[i][k]==term) then
+                    t = t+1
+                end
+            end
+        if (t == #table) then
+            return true
+        else
+            t=0
+        end
     end
-end
-if (t == #table) then
-    return true
-else
-    t=0
-end
-end
-return false
+    return false
 end
 
 function DisplayTable(table)
-for k,v in ipairs(table) do
-for i,v in ipairs(table) do
-    print(table[v][v]) break
-end
-end
+    for k,v in ipairs(table) do
+        for i,v in ipairs(table) do
+            print(table[v][v]) break
+        end
+    end
 end
 
 
